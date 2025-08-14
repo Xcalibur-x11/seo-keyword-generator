b08d80ec3e2513db6da431bc098a82ae3e3a2cf2
Xcalibur-x11 <kazmibrand007@gmail.com>
2025-08-15 00:56:07 +0500

    Add files via upload

---
+++ main.py.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import httpx
+
+app = FastAPI()
+
+@app.get("/keywords")
+async def get_keywords(query: str):
+    url = "https://suggestqueries.google.com/complete/search"
+    params = {"client": "firefox", "q": query}
+    async with httpx.AsyncClient() as client:
+        r = await client.get(url, params=params)
+        return r.json()[1]
